c9902f2614d56739595d1698682f808c5b694982
systematicchaos <passwordtoGH>
2013-07-31 20:28:46 +0300

    asdasd

--- PlanMyDay/PlanMyDay/viewControllers/tasksView/TTTasksTableViewCell.m
+++ PlanMyDay/PlanMyDay/viewControllers/tasksView/TTTasksTableViewCell.m
@@ -24,7 +24,7 @@
 
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
 {
-    //add test changes! passwordtoGH!!!!!!!!13
+    //add test changes! passwordtoGH!!!!!!!!13asd
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
      
