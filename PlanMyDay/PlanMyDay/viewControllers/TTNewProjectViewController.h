858dfd3e6655b085bfb8853af03393f98d8c75fc
systematicChaos <my_regmail@mail.ru>
2013-10-02 20:50:24 +0300

    merge

--- PlanMyDay/PlanMyDay/viewControllers/TTNewProjectViewController.h
+++ PlanMyDay/PlanMyDay/viewControllers/TTNewProjectViewController.h
@@ -10,6 +10,7 @@
 #import "TTApplicationManager.h"
 #import "TTCreatePropertyViewController.h"
 #import "TTSelectPropertyViewController.h"
+#import "TTSelectColorViewController.h"
 #import <QuartzCore/QuartzCore.h>
 
 @interface TTNewProjectViewController : UIViewController <UITextFieldDelegate>
